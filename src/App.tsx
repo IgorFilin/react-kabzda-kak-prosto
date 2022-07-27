432f2548f7597cb83a7ec0c9d97658792715cc69
Igor <filinigor@yandex.ru>
2022-07-27 16:20:54 +0300

    create mySelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import classes from './App.module.css';
 import Accordion from "./components/Accordion/Accordion";
+import {MySelected} from "./components/MySelected/MySelected";
 
 
 export type TypeFilter = false | true
@@ -10,17 +11,18 @@ export type RatingType = 0 | 1 | 2 | 3 | 4 | 5
 function App() {
     //
     // const [rating, setRating] = useState<RatingType>(0)
-    const [accordion, setAccordion] = useState<boolean>(false)
+    // const [accordion, setAccordion] = useState<boolean>(false)
     // const [filtered, setFiltered] = useState(false)
+    const items = [{id: '1', title: 'Igor'}, {id: '2', title: 'Alisa'}, {id: '3', title: 'Vasya'}, {id: '4',title: 'Valera'
+    }, {id: '5', title: 'Bob'}, {id: '6', title: 'Herry'}]
 
+    const testLogIdUser = (id:string) =>{
+        console.log(id)
+    }
 
     return (
         <div className={classes.content}>
-         <Accordion titleValue={'Users'}
-                    accordion={accordion}
-                    setAccordion={()=>setAccordion(!accordion)}
-                    items={[{id:'1',title:'Igor'},{id:'2',title:'Alisa'},{id:'3',title:'Vasya'},{id:'4',title:'Valera'},{id:'5',title:'Bob'},]}
-         />
+            <MySelected itemsName={'users'} items={items} callback={testLogIdUser}/>
         </div>
 
     );
---
+++ src/components/MySelected/MySelected.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {ItemsType} from "../Accordion/Accordion";
+import s from './MySelected.module.css'
+
+type MySelectedPropsType = {
+    itemsName:string
+    items: ItemsType[]
+    callback?: (id: string) => void
+}
+
+export const MySelected = (props: MySelectedPropsType) => {
+    const [collapsed, setCollapsed] = useState(false)
+    const [title, setTitle] = useState(props.itemsName)
+
+    const Collapsed = () => {
+        setCollapsed(!collapsed)
+    }
+    const onClickItemsHahdler = (id: string) => {
+        props.callback && props.callback(id)
+        const newTitle = props.items.find(el => el.id === id)
+        if(newTitle)
+            setTitle(newTitle.title)
+    }
+    const notCollapsed = () => {
+        setCollapsed(false)
+    }
+
+
+
+    return (
+        <div onBlur={notCollapsed} tabIndex={0}  className={s.mySelected}>
+            <div  className={s.title} onClick={Collapsed} >{title}</div>
+
+            {collapsed && props.items.map(el => <div onClick={() => onClickItemsHahdler(el.id)} className={title === el.title ? s.itemActive:s.items} key={el.id}>{el.title}</div>)}
+        </div>
+    );
+};
+
