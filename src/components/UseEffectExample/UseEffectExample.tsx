9417e3925a7d34e4b61f4a6a2646640a38f0cf48
Igor <filinigor@yandex.ru>
2022-08-22 16:41:32 +0300

    (22) create clock in UseEffectExample.tsx

--- src/components/UseEffectExample/UseEffectExample.tsx
+++ src/components/UseEffectExample/UseEffectExample.tsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {clearInterval} from "timers";
 
 export const UseEffectExample = () => {
 
     console.log('rerender component UseEffectExample')
-    const [count,setCount]=useState(1)
+    const [count,setCount]= useState(1)
+    const [fake,fakeCount] = useState(1)
+    const [date,setData]=useState(new Date())
 
     useEffect(() => {
         console.log('rerender all time')
@@ -23,15 +24,42 @@ export const UseEffectExample = () => {
         document.title = count.toString()
     },[count])
 
+    // useEffect(()=>{
+    //     let timerID = setInterval(()=>{
+    //         setCount((state)=> state + 1)//передавай функцию внутрь сетСтейт он не лезет в замыкание и не находит count там,а использует всегда инициализационное значение
+    //     },1000)
+    // },[])
 
     function changeCount(state:number){
         return state + 1
     }
 
+
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+
+
+    const getZero = (number:number) => {
+        if(number >= 0 && number < 10){
+            return '0' + number
+        }else {
+            return number
+        }
+    }
+    const time = getZero(hours)+':'+getZero(minutes)+':'+getZero(seconds)
+
+    useEffect(()=>{
+        setInterval(()=>{
+            setData(new Date())
+        },1000)
+    },[])
     return (
         <div>
-            <button onClick={()=>setCount(changeCount)}>Count</button>
-            {count}
+            <div style={{display:'flex',justifyContent:'center',fontSize:'40px'}}>{time}</div>
+            {/*<button onClick={()=>setCount(changeCount)}>Count</button>*/}
+            {/*<button onClick={()=>fakeCount(changeCount)}>Count</button>*/}
+            {/*{count}*/}
         </div>
     );
 };
